bbb3efb55208d15145d3c97c1b7e8e4bdefced85
Agnish Choudhury <agnish.choudhury@servian.com>
2023-11-02 19:27:12 +0530

    modifications

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,3 +1,12 @@
 provider "azurerm" {
   features {}
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tfstatestorage"
+    storage_account_name  = "tfstatefileagnish"
+    container_name        = "tfstatefile"
+    key                   = "terraform.tfstate"
+  }
+}
