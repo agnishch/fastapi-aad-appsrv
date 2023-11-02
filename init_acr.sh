79a164731b9977cc0a8a2d452f8c3322e12dab10
Agnish Choudhury <agnish.choudhury@servian.com>
2023-11-03 04:34:05 +0530

    .

--- init_acr.sh
+++ init_acr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t agnishacr.azurecr.io/demo-first-img:latest .
-docker push agnishacr.azurecr.io/demo-first-img:latest
+docker build -t agnishacr.azurecr.io/demo-first-img .
+docker push agnishacr.azurecr.io/demo-first-img
