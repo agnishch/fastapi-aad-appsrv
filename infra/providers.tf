6c49f8712207e7f3899da0603ca1fbb3138f2aaf
Agnish Choudhury <agnish.choudhury@servian.com>
2023-11-03 00:51:27 +0530

    .

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,18 +1,17 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-  # backend "azurerm" {
-  #   resource_group_name   = "tfstatestorage"
-  #   storage_account_name  = "tfstatefileagnish"
-  #   container_name        = "tfstatefile"
-  #   key                   = "terraform.tfstate"
-  # }
-}
-
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=3.0.0"
+#     }
+#   }
+#   backend "azurerm" {
+#     resource_group_name   = "tfstatestorage"
+#     storage_account_name  = "tfstatefileagnish"
+#     container_name        = "tfstatefile"
+#     key                   = "terraform.tfstate"
+#   }
+# }
 provider "azurerm" {
   features {}
 }
