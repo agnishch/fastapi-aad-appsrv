761819136f66c0ae74ab286b382efa77aed49860
Agnish Choudhury <agnish.choudhury@servian.com>
2023-11-03 14:47:13 +0530

    debug

--- app/app.py
+++ app/app.py
@@ -5,5 +5,8 @@ app = FastAPI()
 def main():
     return {"Hello": "World"}
 
+@app.get("/sookie")
+def main():
+    return {"Hello": "Sookie"}
 
 # az ad sp create-for-rbac --name fastapi-appsvc --role contributor --scopes /subscriptions/e5c6d7a1-acbb-4854-b6ed-4ca43a989abc
